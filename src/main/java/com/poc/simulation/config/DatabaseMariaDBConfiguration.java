978f41f39b9ebc760f64eee67c27c1f7e3e30905
Julien Ciais <julien.ciais@orange.com>
2022-01-17 22:47:03 +0100

    config: add config dev with H2

---
+++ src/main/java/com/poc/simulation/config/DatabaseMariaDBConfiguration.java
@@ -0,0 +1,18 @@
+package com.poc.simulation.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import tech.jhipster.config.JHipsterConstants;
+
+@Configuration
+@EnableJpaRepositories({ "com.poc.simulation.repository" })
+@EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware")
+@EnableTransactionManagement
+@Profile("local")
+public class DatabaseMariaDBConfiguration {}
